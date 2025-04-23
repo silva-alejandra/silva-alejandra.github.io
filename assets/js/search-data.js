d50f2ca102dd85aa5b90acb5c5e18c9066d2067d
silva-alejandra <ao.silvajane@gmail.com>
2025-04-23 02:02:01 +0000

    Deploying to gh-pages from @ silva-alejandra/silva-alejandra.github.io@a7bb103f3e62dcb737405cbff785e31624c0f093 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Data-driven projects that blend public policy, financial regulation, and technology.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
